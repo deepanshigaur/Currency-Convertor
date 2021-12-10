bf0b4303754abce2449259b04faf51c4de2cb6f9
Deepanshi Gaur <dgaur1@binghamton.edu>
2021-12-10 13:16:17 -0500

    Intial Commit

--- currencies.py
+++ currencies.py
@@ -1,12 +1,12 @@
 import requests
 
 class Currency:
-	def __init__(self):
-		self.api_url = "https://api.frankfurter.app/currencies"
-		self.resp_data = None
+
+    def __init__(self):
+        self.api_url = "https://api.frankfurter.app/currencies"
+        self.resp_data = None
         
-	def get(self):
-		response = requests.get(self.api_url)
-		self.resp_data = response.json()
-		# ERROR HAndling if network failue
-		return self.resp_data
+    def get(self):
+        response = requests.get(self.api_url, timeout=5)
+        self.resp_data = response.json()
+
