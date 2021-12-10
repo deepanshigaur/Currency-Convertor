6a58fbb186d0ed7d7e809e908d3d95ec0ac44aed
Deepanshi Gaur <dgaur1@binghamton.edu>
2021-12-10 14:11:04 -0500

    Initial Commit

---
+++ consts.py
@@ -0,0 +1,4 @@
+SUPPORTED_CURRENCIES = ["AUD", "BGN", "BRL", "CAD", "CHF", "CNY",
+ "CZK", "DKK", "EUR", "GBP", "HKD", "HRK", "HUF", "IDR", "ILS", "INR", 
+ "ISK", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PLN", "RON", "USD"]
+
